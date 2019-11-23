72d315e1e6f90e94043ab59526df79a1084c4cfa
KJBose <2bose.on@gmail.com>
2019-11-22 16:46:56 -0800

    Create ELSTM.py
    Temp File.. to develop further

---
+++ EmotionRecognizer/ELSTM.py
@@ -0,0 +1,15 @@
+import Keras as ke
+import tensorflow as tf
+import matplotlib as plt
+import numpy as np
+import scikitlearn as sk
+
+def elstm_model():
+    print("stub")
+
+
+def train():
+    print("stub):
+   
+def validate():
+    print("stub")
